25912dfa5a25bdd7c53fb98a190506140e21754a
demain_lee <lmm_work@163.com>
2023-08-18 13:22:46 +0800

    :sparkles: 接口文档配置

--- order-server/src/main/java/com/demain/config/OpenApiConfig.java
+++ order-server/src/main/java/com/demain/config/OpenApiConfig.java
@@ -1,20 +1,46 @@
 package com.demain.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 接口文档配置
+ *
+ * @author demain_lee
+ * @since 2023/8/18
+ */
 @Configuration
 public class OpenApiConfig {
 
-    @Bean
-    public OpenAPI springOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Order API")
-                        .description("Order API Description")
-                        .version("0.0.1"));
-    }
+	@Bean
+	public OpenAPI openApi() {
+		//	@formatter:off
+		return new OpenAPI()
+				.info(apiInfo())
+				;
+		// @formatter:on
+	}
+
+	public Info apiInfo() {
+		//	@formatter:off
+		return new Info()
+				.title("Order API")
+				.description("Order API Description")
+				.contact(new Contact()
+						.name("demain_lee")
+						.email("lmm_work@163.com")
+						.url("https://xxxxxxxx.com"))
+				.license(new License()
+						.name("Apache 2.0")
+						.url("http://www.apache.org/licenses/LICENSE-2.0.html"))
+				.version("0.0.1")
+
+				// @formatter:on
+		;
+	}
 
 }
